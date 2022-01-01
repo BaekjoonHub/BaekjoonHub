17c8bccda5ca02e6ab78d040399780d396fcef4f
getCurrentThread <wak8835@gmail.com>
2022-01-01 17:00:12 +0900

    Feat XMLHttpRequest API 를 Fetch API로 대체 및 Promise 병렬 로직 처리 추가 (#16)

--- scripts/baekjoon/keyfunctions.js
+++ scripts/baekjoon/keyfunctions.js
@@ -1,26 +1,19 @@
 /* Main function for uploading code to GitHub repo, and callback cb is called if success */
+// 업로드 함수 - 신규 코드 커밋에 사용됨
 const upload = (token, hook, code, directory, filename, sha, msg, cb = undefined) => {
   // To validate user, load user object from GitHub.
-  const URL = `https://api.github.com/repos/${hook}/contents/${directory}/${filename}`;
-
-  console.log(`directory is ${directory}, file name is ${filename}`);
-
-  /* Define Payload */
-  let data = {
-    message: msg,
-    content: code,
-    sha,
-  };
-
-  data = JSON.stringify(data);
-
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('readystatechange', function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200 || xhr.status === 201) {
-        const { sha } = JSON.parse(xhr.responseText).content; // get updated SHA.
-        chrome.storage.local.get('stats', (data2) => {
-          let { stats } = data2;
+  fetch(`https://api.github.com/repos/${hook}/contents/${directory}/${filename}`, {
+    method: 'PUT',
+    body: JSON.stringify({ message: msg, content: code, sha }),
+    headers: { Authorization: `token ${token}`, Accept: 'application/vnd.github.v3+json' },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (debug) console.log('data', data);
+      if (data != null || (data != data.content) != null || data.content.sha != null) {
+        const { sha } = data.content; // get updated SHA.
+        chrome.storage.local.get('stats', (data) => {
+          let { stats } = data;
           if (stats === null || stats === {} || stats === undefined) {
             // create stats object
             stats = {};
@@ -31,16 +24,13 @@ const upload = (token, hook, code, directory, filename, sha, msg, cb = undefined
             stats.platinum = 0;
             stats.diamond = 0;
             stats.ruby = 0;
-
             stats.submission = {};
           }
           const filePath = bojData.meta.problemId + bojData.meta.problemId + bojData.meta.language;
-
           const { submissionId } = bojData.submission;
           stats.submission[filePath] = { submissionId, sha }; // update sha key.
           chrome.storage.local.set({ stats }, () => {
             if (debug) console.log(`Successfully committed ${filename} to github`);
-
             // if callback is defined, call it
             if (cb !== undefined) {
               cb();
@@ -48,50 +38,35 @@ const upload = (token, hook, code, directory, filename, sha, msg, cb = undefined
           });
         });
       }
-    }
-  });
-  xhr.open('PUT', URL, true);
-  xhr.setRequestHeader('Authorization', `token ${token}`);
-  xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-  xhr.send(data);
+    });
 };
-
 /* Main function for updating code on GitHub Repo */
 /* Currently only used for prepending discussion posts to README */
 /* callback cb is called on success if it is defined */
+// 업데이트 함수
 const update = (token, hook, addition, directory, msg, prepend, cb = undefined) => {
-  const URL = `https://api.github.com/repos/${hook}/contents/${directory}/README.md`;
-
-  /* Read from existing file on GitHub */
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener(' ', function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200 || xhr.status === 201) {
-        const response = JSON.parse(xhr.responseText);
-        const existingContent = b64DecodeUnicode(response.content);
-        let newContent = '';
-
-        /* Discussion posts prepended at top of README */
-        /* Future implementations may require appending to bottom of file */
-        if (prepend) {
-          newContent = b64EncodeUnicode(addition + existingContent);
-        }
-
-        /* Write file with new content to GitHub */
-        upload(token, hook, newContent, directory, 'README.md', response.sha, msg, cb);
+  fetch(`https://api.github.com/repos/${hook}/contents/${directory}/README.md`, {
+    method: 'GET',
+    headers: { Authorization: `token ${token}`, Accept: 'application/vnd.github.v3+json' },
+  })
+    .then((res) => res.json())
+    .data((response) => {
+      const existingContent = b64DecodeUnicode(response.content);
+      let newContent = '';
+      /* Discussion posts prepended at top of README */
+      /* Future implementations may require appending to bottom of file */
+      if (prepend) {
+        newContent = b64EncodeUnicode(addition + existingContent);
       }
-    }
-  });
-  xhr.open('GET', URL, true);
-  xhr.setRequestHeader('Authorization', `token ${token}`);
-  xhr.setRequestHeader('Accept', 'application/vnd.github.v3+json');
-  xhr.send();
+      /* Write file with new content to GitHub */
+      upload(token, hook, newContent, directory, 'README.md', response.sha, msg, cb);
+    });
 };
-
 function uploadGit(code, problemName, fileName, msg, action, prepend = true, cb = undefined) {
   /* Get necessary payload data */
   chrome.storage.local.get('BaekjoonHub_token', (t) => {
     const token = t.BaekjoonHub_token;
+    if (debug) console.log('token', token);
     if (token) {
       chrome.storage.local.get('mode_type', (m) => {
         const mode = m.mode_type;
@@ -101,13 +76,11 @@ function uploadGit(code, problemName, fileName, msg, action, prepend = true, cb 
             const hook = h.BaekjoonHub_hook;
             if (hook) {
               /* Get SHA, if it exists */
-
               /* to get unique key */
               const filePath = problemName + fileName;
               chrome.storage.local.get('stats', (s) => {
                 const { stats } = s;
                 let sha = null;
-
                 if (stats !== undefined && stats.submission !== undefined && stats.submission[filePath] !== undefined) {
                   sha = stats.submission[filePath].sha;
                 }
