81a993f40b1923bb3b1ac2010f4abdcc75395723
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-01-01 23:24:05 +0900

    코멘트 삭제

--- scripts/baekjoon/parsing.js
+++ scripts/baekjoon/parsing.js
@@ -75,27 +75,6 @@ function findWithPromise() {
   }
 }
 
-// function findSolvedAPI() {
-//   const levelxhttp = new XMLHttpRequest();
-//   levelxhttp.onreadystatechange = function () {
-//     if (this.readyState === 4 && this.status === 200) {
-//       /* received submission details as html reponse. */
-//       const leveldoc = JSON.parse(this.response);
-//       bojData.meta.title = `${leveldoc.titleKo}`;
-//       bojData.meta.level = bj_level[leveldoc.level];
-//       bojData.meta.directory = `백준/${bojData.meta.level.replace(/ .*/, '')}/${bojData.meta.problemId}.${bojData.meta.title.replace(/\s+/g, '-').replace(titleRegex, '')}`;
-//       bojData.meta.message = `[${bojData.meta.level}] Title: ${bojData.meta.title}, Time: ${bojData.submission.runtime} ms, Memory: ${bojData.submission.memory} KB -BaekjoonHub`;
-//       let string = '';
-//       leveldoc.tags.forEach((tag) => (string += `${categories[tag.key]}(${tag.key}), `));
-//       console.log(string.length);
-//       const { length } = string;
-//       bojData.meta.category = string.substring(0, length - 2);
-//       if (debug) console.log(leveldoc);
-//     }
-//   };
-//   levelxhttp.open('GET', `https://solved.ac/api/v3/problem/show?problemId=${bojData.meta.problemId}`, false);
-//   levelxhttp.send();
-// }
 // Get Submission Number
 function findSubmissionId() {
   const problemElem = document.getElementById('status-table').childNodes[1].childNodes[0].childNodes[0];
