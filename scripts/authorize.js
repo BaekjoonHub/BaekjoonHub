fe67382287aab137bd0c5241335efb1ce7b01c41
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-01-01 23:26:11 +0900

    refactor: 백준 파싱의 HTTP 요청을 XHR 에서 Fetch 병렬 호출로 전환 (#17)
    - 문제 설명·제출 코드·solved.ac 조회를 Promise.all 로 동시에 요청하는 findWithPromise 로 재작성
    - bojData 검증을 재귀 isNotEmpty 로 교체하고 HTML 이스케이프/언이스케이프 유틸 추가
    - prettier printWidth 400·eslint endOfLine crlf 설정에 맞춰 스크립트 전체 재포맷
    - package-lock.json 을 lockfileVersion 1 로 재생성
    - 1.0.2 패치노트 예시의 문제 번호·파일명 오타 수정
    
    Co-authored-by: getCurrentThread <wak8835@gmail.com>

--- scripts/authorize.js
+++ scripts/authorize.js
@@ -9,10 +9,8 @@ const localAuth = {
    */
   init() {
     this.KEY = 'leethub_token';
-    this.ACCESS_TOKEN_URL =
-      'https://github.com/login/oauth/access_token';
-    this.AUTHORIZATION_URL =
-      'https://github.com/login/oauth/authorize';
+    this.ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+    this.AUTHORIZATION_URL = 'https://github.com/login/oauth/authorize';
     this.CLIENT_ID = 'beb4f0aa19ab8faf5004';
     this.CLIENT_SECRET = '843f835609c7ef02ef0f2f1645bc49514c0e65a6';
     this.REDIRECT_URL = 'https://github.com/'; // for example, https://github.com
@@ -51,9 +49,7 @@ const localAuth = {
     xhr.addEventListener('readystatechange', function () {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
-          that.finish(
-            xhr.responseText.match(/access_token=([^&]*)/)[1],
-          );
+          that.finish(xhr.responseText.match(/access_token=([^&]*)/)[1]);
         } else {
           chrome.runtime.sendMessage({
             closeWebPage: true,
--- scripts/baekjoon/util.js
+++ scripts/baekjoon/util.js
@@ -1,15 +1,15 @@
 // Upload icon - Set Loading Icon
 /* start upload will inject a spinner on left side to the "Run Code" button */
 function startUpload() {
-  elem = document.getElementById('BaekjoonHub_progress_anchor_element')
+  let elem = document.getElementById('BaekjoonHub_progress_anchor_element');
   if (elem !== undefined) {
-    elem = document.createElement('span')
-    elem.id = "BaekjoonHub_progress_anchor_element"
-    elem.className = "runcode-wrapper__8rXm"
-    elem.style = "margin-left: 10px;padding-top: 0px;"
+    elem = document.createElement('span');
+    elem.id = 'BaekjoonHub_progress_anchor_element';
+    elem.className = 'runcode-wrapper__8rXm';
+    elem.style = 'margin-left: 10px;padding-top: 0px;';
   }
-  elem.innerHTML = `<div id="BaekjoonHub_progress_elem" class="BaekjoonHub_progress"></div>`
-  target = document.getElementById("status-table").childNodes[1].childNodes[0].childNodes[3];
+  elem.innerHTML = `<div id="BaekjoonHub_progress_elem" class="BaekjoonHub_progress"></div>`;
+  const target = document.getElementById('status-table').childNodes[1].childNodes[0].childNodes[3];
   if (target.childNodes.length > 0) {
     target.childNodes[0].append(elem);
   }
@@ -20,23 +20,20 @@ function startUpload() {
 // Upload icon - Set Completed Icon
 /* This will create a tick mark before "Run Code" button signalling BaekjoonHub has done its job */
 function markUploaded() {
-  elem = document.getElementById("BaekjoonHub_progress_elem");
-  elem.className = "";
-  style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid #78b13f;border-right:3px solid #78b13f;'
+  const elem = document.getElementById('BaekjoonHub_progress_elem');
+  elem.className = '';
+  const style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid #78b13f;border-right:3px solid #78b13f;';
   elem.style = style;
 }
 
 // Upload icon - Set Failed Icon
 /* This will create a failed tick mark before "Run Code" button signalling that upload failed */
 function markUploadFailed() {
-  elem = document.getElementById("BaekjoonHub_progress_elem");
-  elem.className = "";
-  style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid red;border-right:3px solid red;'
+  const elem = document.getElementById('BaekjoonHub_progress_elem');
+  elem.className = '';
+  const style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid red;border-right:3px solid red;';
   elem.style = style;
 }
-
-
-
 
 /* inject css style required for the upload progress feature */
 function injectStyle() {
@@ -59,17 +56,81 @@ function checkElem(elem) {
   return elem && elem.length > 0;
 }
 
-function ready(){
-  if(bojData.meta.title === '') return false;
-  if(bojData.meta.problemId === '') return false;
-  if(bojData.meta.level === '') return false;
-  if(bojData.meta.problemDescription === '') return false;
-  if(bojData.meta.language === '') return false;
-  if(bojData.meta.message === '') return false;
-  if(bojData.meta.fileName === '') return false;
-  if(bojData.submission.submissionId === '') return false;
-  if(bojData.submission.code === '') return false;
-  if(bojData.submission.memory === '') return false;
-  if(bojData.submission.runtime === '') return false;
+/* A function that recursively checks that all values of object are not '' */
+function isNotEmpty(obj) {
+  if (obj === undefined || obj === null || obj === '' || obj === [] || obj === {}) return false;
+  if (typeof obj !== 'object') return true;
+  if (obj.length === 0) return false;
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      if (!isNotEmpty(obj[key])) return false;
+    }
+  }
   return true;
+}
+
+function ready() {
+  if (debug) {
+    console.log('ready', bojData);
+  }
+  return isNotEmpty(bojData);
+}
+
+function escapeHtml(text) {
+  const map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;',
+  };
+
+  return text.replace(/[&<>"']/g, function (m) {
+    return map[m];
+  });
+}
+
+function unescapeHtml(text) {
+  const unescaped = {
+    '&amp;': '&',
+    '&#38;': '&',
+    '&lt;': '<',
+    '&#60;': '<',
+    '&gt;': '>',
+    '&#62;': '>',
+    '&apos;': "'",
+    '&#39;': "'",
+    '&quot;': '"',
+    '&#34;': '"',
+  };
+  return text.replace(/&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34);/g, function (m) {
+    return unescaped[m];
+  });
+}
+
+String.prototype.escapeHtml = function () {
+  return escapeHtml(this);
+};
+
+String.prototype.unescapeHtml = function () {
+  return unescapeHtml(this);
+};
+
+function b64EncodeUnicode(str) {
+  return btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+      return String.fromCharCode(`0x${p1}`);
+    }),
+  );
+}
+
+function b64DecodeUnicode(b64str) {
+  return decodeURIComponent(
+    atob(b64str)
+      .split('')
+      .map(function (c) {
+        return `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`;
+      })
+      .join(''),
+  );
 }
