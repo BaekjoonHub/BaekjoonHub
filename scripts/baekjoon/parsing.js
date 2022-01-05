13c4d6800284b717a1c4abbd842b5937fa740c10
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-01-05 13:54:40 +0900

    feat: 백준 최신 코드 업로드와 버전 불일치 시 업데이트 안내 추가 (#21)
    - findData() 가 제출 데이터를 반환하도록 재구성하고 README·코드 SHA 를 타입별로 저장해 신규 제출마다 최신 풀이를 올리도록 변경
    - 저장된 stats 버전이 다르면 업로드를 멈추고 안내 후 업데이트 버튼을 삽입하는 UpdateForNewVersion.js 추가
    - 업로드 진행·성공·실패 표시 스타일을 css/inject.css 로 분리해 콘텐츠 스크립트에 주입
    - popup·welcome 의 HTML/CSS 재포맷, package-lock.json 을 lockfileVersion 2 로 재생성
    - 패치노트·README·TODO 갱신
    
    Refs: #19, #20
    
    Co-authored-by: getCurrentThread <wak8835@gmail.com>

--- scripts/baekjoon/parsing.js
+++ scripts/baekjoon/parsing.js
@@ -1,103 +1,212 @@
-// Parse all BOJ Data
-function findData() {
-  // language, submisssionId, problemId, memory, runtime
-  findFromResultTable();
-  // problemDescription
-  findWithPromise();
+/*
+  문제가 맞았다면 문제 관련 데이터를 파싱하는 함수의 모음입니다.
+  모든 해당 파일의 모든 함수는 findData()를 통해 호출됩니다.
+*/
 
-  console.log(bojData);
+/*
+  bojData를 초기화하는 함수로 문제 요약과 코드를 파싱합니다.
+
+  - 문제 설명: problemDescription
+  - Github repo에 저장될 디렉토리: directory
+  - 커밋 메시지: message 
+  - 백준 문제 카테고리: category
+  - 파일명: fileName
+  - Readme 내용 : readme
+*/
+async function findData() {
+  const bojData = {
+    // Meta data of problem
+    meta: {
+      title: '',
+      problemId: '',
+      level: '',
+      problemDescription: '',
+      language: '',
+      message: '',
+      fileName: '',
+      category: '',
+      readme: '',
+      directory: '',
+    },
+    submission: {
+      submissionId: '',
+      code: '',
+      memory: '',
+      runtime: '',
+    },
+  };
+
+  try {
+    const { 
+      username, 
+      result, 
+      memory, 
+      runtime, 
+      language, 
+      submissionTime, 
+      submissionId, 
+      problemId 
+    } = findFromResultTable();
+    const {
+      title, 
+      level, 
+      code,
+      tags,
+      problem_description, 
+      problem_input, 
+      problem_output 
+    } = await findProblemDetailsAndSubmissionCode(problemId, submissionId);
+    const problemDescription = `### 문제 설명\n\n${problem_description}\n\n`
+                            + `### 입력 \n\n ${problem_input}\n\n`
+                            + `### 출력 \n\n ${problem_output}\n\n`;
+    const directory = `백준/${level.replace(/ .*/, '')}/${problemId}.${title.replace(/\s+/g, '-').replace(titleRegex, '')}`;
+    const message = `[${level}] Title: ${title}, Time: ${runtime} ms, Memory: ${memory} KB -BaekjoonHub`;
+    const tagl = [];
+    tags.forEach((tag) => tagl.push(`${categories[tag.key]}(${tag.key})`));
+    const category = tagl.join(', ');
+    const fileName = title.replace(/\s+/g, '-').replace(titleRegex, '') + languages[language];
+    const readme = `# [${level}] ${title} - ${problemId} \n\n` 
+                + `[문제 링크](https://www.acmicpc.net/problem/${problemId}) \n\n`
+                + `### 성능 요약\n\n`
+                + `메모리: ${memory} KB, `
+                + `시간: ${runtime} ms\n\n`
+                + `### 분류\n\n`
+                + `${category}\n\n`
+                + `${problemDescription}\n\n`;
+    return {
+      meta: {
+        title,
+        problemId,
+        level,
+        problemDescription,
+        language,
+        message,
+        fileName,
+        category,
+        readme,
+        directory,
+      },
+      submission: {
+        submissionId,
+        code,
+        memory,
+        runtime,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
+  return bojData;
 }
 
+function findUsername() {
+  return document.querySelector('a.username').innerText;
+}
+
+function isExistResultTable() {
+  return document.getElementById('status-table') !== null;
+}
+
+function findResultTableList() {
+  const table = document.getElementById('status-table');
+  if (table === null || table === undefined || table.length === 0) return [];
+  const headers = Array.from(table.rows[0].cells, (x) => convertResultTableHeader(x.innerText.trim()));
+
+  const list = [];
+  for (let i = 1; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    const cells = Array.from(row.cells, (x, index) => {
+      switch (headers[index]) {
+        case 'language':
+          return x.innerText.unescapeHtml().replace(/\/.*$/g, '').trim();
+        case 'submissionTime':
+          return x.firstChild.getAttribute('data-original-title');
+        default:
+          return x.innerText.trim();
+      }
+    });
+    const obj = {};
+    obj.element = row;
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = cells[j];
+    }
+    list.push(obj);
+  }
+  if (debug) console.log('TableList', list);
+  return list;
+}
+
+/*
+  제출 화면의 데이터를 파싱하는 함수로 다음 데이터를 확인합니다.
+    - 유저이름: username
+    - 실행결과: result
+    - 메모리: memory
+    - 실행시간: runtime
+    - 제출언어: language
+    - 제출시간: submissionTime
+    - 제출번호: submissionId
+    - 문제번호: problemId
+    - 해당html요소 : element
+*/
 function findFromResultTable() {
-  const submissionEle = document.getElementById('status-table').childNodes[1].childNodes[0];
-  bojData.submission.memory = submissionEle.childNodes[4].innerText;
-  bojData.submission.runtime = submissionEle.childNodes[5].innerText;
-  bojData.meta.language = submissionEle.childNodes[6].childNodes[0].innerHTML;
-  findSubmissionId();
-  findProblemId();
+  if (!isExistResultTable()) {
+    if (debug) console.log('Result table not found');
+  }
+  const resultList = findResultTableList();
+  if (resultList.length === 0) return;
+  const row = resultList[0];
+  return row;
 }
 
-function findWithPromise() {
+/*
+  Fetch를 사용하여 정보를 구하는 함수로 다음 정보를 확인합니다.
+
+    - 문제 설명: problem_description
+    - 문제 입력값: problem_input
+    - 문제 출력값: problem_output
+    - 제출 코드: code
+    - 문제 제목: title
+    - 문제 등급: level 
+    - Github repo에 저장될 디렉토리: directory
+    - 커밋 메시지: message 
+    - 백준 문제 카테고리: category
+*/
+
+async function findProblemDetailsAndSubmissionCode(problemId, submissionId) {
   if (debug) console.log('in find with promise');
-  if (checkElem(bojData.meta.problemId) && checkElem(bojData.submission.submissionId)) {
-    const DescriptionParse = fetch(`https://www.acmicpc.net/problem/${bojData.meta.problemId}`, { method: 'GET' });
-    const CodeParse = fetch(`https://www.acmicpc.net/source/download/${bojData.submission.submissionId}`, { method: 'GET' });
-    const SolvedAPI = fetch(`https://solved.ac/api/v3/problem/show?problemId=${bojData.meta.problemId}`, { method: 'GET' });
-    Promise.all([DescriptionParse, CodeParse, SolvedAPI])
+  if (checkElem(problemId) && checkElem(submissionId)) {
+    const DescriptionParse = fetch(`https://www.acmicpc.net/problem/${problemId}`, { method: 'GET' });
+    const CodeParse = fetch(`https://www.acmicpc.net/source/download/${submissionId}`, { method: 'GET' });
+    const SolvedAPI = fetch(`https://solved.ac/api/v3/problem/show?problemId=${problemId}`, { method: 'GET' });
+    return Promise.all([DescriptionParse, CodeParse, SolvedAPI])
       .then(([despResponse, codeResponse, solvedResponse]) => Promise.all([despResponse.text(), codeResponse.text(), solvedResponse.json()]))
       .then(([descriptionText, codeText, solvedJson]) => {
         /* Get Question Description */
-        let questionDescription = '';
         const parser = new DOMParser();
         const doc = parser.parseFromString(descriptionText, 'text/html');
+        let problem_description;
+        let problem_input;
+        let problem_output;
         if (doc != null) {
-          // eslint-disable-next-line prettier/prettier
-          questionDescription = `### 문제 설명\n\n${unescapeHtml(doc.getElementById('problem_description').innerHTML.trim())}\n\n`
-                              + `### 입력 \n\n ${unescapeHtml(doc.getElementById('problem_input').innerHTML.trim())}\n\n`
-                              + `### 출력 \n\n ${unescapeHtml(doc.getElementById('problem_output').innerHTML.trim())}\n\n`;
+          problem_description = `${unescapeHtml(doc.getElementById('problem_description').innerHTML.trim())}`;
+          problem_input =       `${unescapeHtml(doc.getElementById('problem_input').innerHTML.trim())}`;
+          problem_output =      `${unescapeHtml(doc.getElementById('problem_output').innerHTML.trim())}`;
         }
-        bojData.meta.problemDescription = questionDescription;
-        if (debug) console.log('findWithPromise - description', questionDescription);
         /* Get Code */
-        bojData.submission.code = codeText;
-        if (debug) console.log('findWithPromise - code', codeText);
+        const code = codeText;
+        if (debug) console.log('findWithPromise - code', code);
         /* Get Solved Response */
-        bojData.meta.title = solvedJson.titleKo;
-        bojData.meta.level = bj_level[solvedJson.level];
-        bojData.meta.directory = `백준/${bojData.meta.level.replace(/ .*/, '')}/${bojData.meta.problemId}.${bojData.meta.title.replace(/\s+/g, '-').replace(titleRegex, '')}`;
-        bojData.meta.message = `[${bojData.meta.level}] Title: ${bojData.meta.title}, Time: ${bojData.submission.runtime} ms, Memory: ${bojData.submission.memory} KB -BaekjoonHub`;
-        let str = '';
-        solvedJson.tags.forEach((tag) => (str += `${categories[tag.key]}(${tag.key}), `));
-        if (debug) console.log(str.length);
-        const { length } = str;
-        bojData.meta.category = str.substring(0, length - 2);
-        if (debug) console.log('findWithPromise - leveldoc', solvedJson);
-        /* Create Readme */
-        bojData.meta.fileName = bojData.meta.title.replace(/\s+/g, '-').replace(titleRegex, '') + languages[bojData.meta.language];
-        // eslint-disable-next-line prettier/prettier
-        bojData.meta.readme = `# [${bojData.meta.level}] ${bojData.meta.title} - ${bojData.meta.problemId} \n\n` 
-                            + `[문제 링크](https://www.acmicpc.net/problem/${bojData.meta.problemId}) \n\n`
-                            + `### 성능 요약\n\n`
-                            + `메모리: ${bojData.submission.memory} KB, `
-                            + `시간: ${bojData.submission.runtime} ms\n\n`
-                            + `### 분류\n\n`
-                            + `${bojData.meta.category}\n\n`
-                            + `${bojData.meta.problemDescription}\n\n`;
-
-        if (debug) console.log('findData', bojData);
-        beginUpload();
-      })
-      .catch((err) => {
-        console.log('error ocurred: ', err);
-        uploadState.uploading = false;
-        markUploadFailed();
+        const { tags } = solvedJson;
+        const title = solvedJson.titleKo;
+        const level = bj_level[solvedJson.level];
+        return { problemId, submissionId, title, level, tags, code, problem_description, problem_input, problem_output };
       });
+    // .catch((err) => {
+    //   console.log('error ocurred: ', err);
+    //   uploadState.uploading = false;
+    //   markUploadFailedCSS();
+    // });
   }
-}
-
-// Get Submission Number
-function findSubmissionId() {
-  const problemElem = document.getElementById('status-table').childNodes[1].childNodes[0].childNodes[0];
-  bojData.submission.submissionId = problemElem.innerText;
-}
-
-function findProblemId() {
-  const problemElem = document.getElementById('status-table').childNodes[1].childNodes[0].childNodes[2];
-  console.log("findProblem", problemElem);
-  if (debug) console.log('getProblemId: ');
-  if (debug) console.log(problemElem);
-  let resultId = '';
-  for (let i = 0; i <= problemElem.childElementCount; i++) {
-    const temp_name = problemElem.childNodes[i];
-    if (temp_name == null || temp_name == 'undefined' || temp_name == undefined) continue;
-    const temp_text = temp_name.innerHTML;
-    if (temp_text.length > resultId.length) {
-      if (debug) console.log(`adding: ${temp_text}`);
-      resultId = temp_text;
-    }
-  }
-  if (debug) console.log(resultId);
-  bojData.meta.problemId = resultId;
 }
 
 /* Since we dont yet have callbacks/promises that helps to find out if things went bad */
@@ -108,7 +217,7 @@ function startUploadCountDown() {
     if (uploadState.uploading === true) {
       // still uploading, then it failed
       uploadState.uploading = false;
-      markUploadFailed();
+      markUploadFailedCSS();
     }
   }, 10000);
 }
--- scripts/baekjoon/util.js
+++ scripts/baekjoon/util.js
@@ -19,36 +19,20 @@ function startUpload() {
 
 // Upload icon - Set Completed Icon
 /* This will create a tick mark before "Run Code" button signalling BaekjoonHub has done its job */
-function markUploaded() {
+function markUploadedCSS() {
   const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = '';
-  const style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid #78b13f;border-right:3px solid #78b13f;';
-  elem.style = style;
+  elem.className = 'markuploaded';
 }
 
 // Upload icon - Set Failed Icon
 /* This will create a failed tick mark before "Run Code" button signalling that upload failed */
-function markUploadFailed() {
+function markUploadFailedCSS() {
   const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = '';
-  const style = 'display: inline-block;transform: rotate(45deg);height:13px;width:5px;border-bottom:3px solid red;border-right:3px solid red;';
-  elem.style = style;
+  elem.className = 'markuploadfailed';
 }
 
-/* inject css style required for the upload progress feature */
-function injectStyle() {
-  const style = document.createElement('style');
-  style.textContent = '.BaekjoonHub_progress {\
-    display: inline-block; \
-    pointer-events: none; \
-    width: 0.8em; \
-    height: 0.8em; \
-    border: 0.4em solid transparent; \
-    border-color: #eee; \
-    border-top-color: #3E67EC; \
-    border-radius: 50%; \
-    animation: loadingspin 1.0s linear infinite; } @keyframes loadingspin { 100% { transform: rotate(360deg) }}';
-  document.head.append(style);
+function getVersion() {
+  return chrome.runtime.getManifest().version;
 }
 
 /* Util function to check if an element exists */
@@ -67,13 +51,6 @@ function isNotEmpty(obj) {
     }
   }
   return true;
-}
-
-function ready() {
-  if (debug) {
-    console.log('ready', bojData);
-  }
-  return isNotEmpty(bojData);
 }
 
 function escapeHtml(text) {
@@ -102,8 +79,10 @@ function unescapeHtml(text) {
     '&#39;': "'",
     '&quot;': '"',
     '&#34;': '"',
+    '&nbsp;': ' ',
+    '&#160;': ' ',
   };
-  return text.replace(/&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34);/g, function (m) {
+  return text.replace(/&(?:amp|#38|lt|#60|gt|#62|apos|#39|quot|#34|nbsp|#160);/g, function (m) {
     return unescaped[m];
   });
 }
@@ -133,4 +112,35 @@ function b64DecodeUnicode(b64str) {
       })
       .join(''),
   );
+}
+
+function convertResultTableHeader(header) {
+  switch (header) {
+    case '문제번호':
+    case '문제':
+      return 'problemId';
+    case '난이도':
+      return 'level';
+    case '결과':
+      return 'result';
+    case '문제내용':
+      return 'problemDescription';
+    case '언어':
+      return 'language';
+    case '제출 번호':
+      return 'submissionId';
+    case '아이디':
+      return 'username';
+    case '제출시간':
+    case '제출한 시간':
+      return 'submissionTime';
+    case '시간':
+      return 'runtime';
+    case '메모리':
+      return 'memory';
+    case '코드 길이':
+      return 'codeLength';
+    default:
+      return 'unknown';
+  }
 }
