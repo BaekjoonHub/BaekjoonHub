ce207fbccd67c2fd8ad17475a383d264e76ce284
getCurrentThread <wak8835@gmail.com>
2022-02-24 16:45:27 +0900

    Feat 프로그래머스 기본 로더가 동작할 때에 로딩 상황이 나오도록 추가

--- scripts/programmers/programmers.js
+++ scripts/programmers/programmers.js
@@ -9,25 +9,21 @@ let loader;
 
 const currentUrl = window.location.href;
 
-// 프로그래머스 연습 문제 주소임을 확인하고, 맞다면 로더를 실행한다
-if (currentUrl.includes('/learn/courses') && currentUrl.includes('lessons')) startLoader();
+// 프로그래머스 연습 문제 주소임을 확인하고, 맞다면 로더를 실행
+if (currentUrl.includes('/learn/courses/30') && currentUrl.includes('lessons')) startLoader();
 
 function startLoader() {
   loader = setInterval(async () => {
     // 제출 후 채점하기 결과가 성공적으로 나왔다면 코드를 파싱하고, 업로드를 시작한다
-    if (get_solved_result().includes('정답')) {
+    if (getSolvedResult().includes('정답')) {
       if (debug) console.log("정답이 나왔습니다. 업로드를 시작합니다.");
       stopLoader();
-      // parsing code, and upload github
-      // 이후에 은근 딜레이가 있어서, 다른 사람이 맞은 현황으로 페이지 이동을 자동으로 해줘야할 것 같습니다.
-      // 아래는 파싱할 수 있는 항목들 종합입니다.
-      // 문제 번호 : document.querySelector("div.main > div.lesson-content").getAttribute("lesson-id")
-      // 문제 제목 : document.querySelector("#tab > li.algorithm-title").textContent.replace(/\\n/g, '').trim()
-      // 문제 내용 : document.querySelector("div.guide-section-description > div.markdown").innerHTML
-      // 작성 언어 : document.querySelector("div.editor > ul > li.nav-item > a").getAttribute("data-language")
-      // 작성 코드 : [...document.querySelectorAll("pre.CodeMirror-line")].map(x => x.innerText).reduce( (x, y) => x + '\n' + y);
-      // 체점 결과 : [...document.querySelectorAll("#output > pre.console-content > div.console-message")].map(x => x.innerText).filter(x => x.includes("합계: ")).reduce( (x, y) => x + '\n' + y)
-      // 시용 메모리 & 시간측정 : let [time, memory] = [...document.querySelectorAll("td.result.passed")].map(x => x.innerText).map(x => x.replace(/[^\., 0-9a-zA-Z]/g, '').trim()).map(x => x.split(", ")).reduce( (x, y) => Number(x[0]) > Number(y[0]) ? x : y)
+      try {
+        const bojData = await parseData();
+        await beginUpload(bojData);
+      } catch (error) {
+        if (debug) console.log(error);
+      }
     }
   }, 2000);
 }
@@ -36,11 +32,48 @@ function stopLoader() {
   clearInterval(loader);
 }
 
-function get_solved_result() {
+function getSolvedResult() {
   const result = document.querySelector('div.modal-header > h4');
   if (result) return result.innerText;
   return '';
 }
+
+/* 파싱 직후 실행되는 함수 */
+async function beginUpload(bojData) {
+  if (debug) console.log('bojData', bojData);
+  if (isNotEmpty(bojData)) {
+    startUpload();
+
+    const stats = await getStats();
+    const hook = await getHook();
+
+    const currentVersion = stats.version;
+    /* 버전 차이가 발생하거나, 해당 hook에 대한 데이터가 없는 경우 localstorage의 Stats 값을 업데이트하고, version을 최신으로 변경한다 */
+    if (isNull(currentVersion) || currentVersion !== getVersion() || isNull(await getStatsSHAfromPath(hook))) {
+      await versionUpdate();
+    }
+
+    /* 현재 제출하려는 소스코드가 기존 업로드한 내용과 같다면 중지 */
+    if (debug) console.log('local:', await getStatsSHAfromPath(`${hook}/${bojData.directory}/${bojData.fileName}`), 'calcSHA:', calculateBlobSHA(bojData.code));
+    if ((await getStatsSHAfromPath(`${hook}/${bojData.directory}/${bojData.fileName}`)) === calculateBlobSHA(bojData.code)) {
+      markUploadedCSS();
+      console.log(`현재 제출번호를 업로드한 기록이 있습니다. submissionID ${bojData.problemId}`);
+      return;
+    }
+    /* 신규 제출 번호라면 새롭게 커밋  */
+    await uploadOneSolveProblemOnGit(bojData, markUploadedCSS);
+  }
+}
+
+async function versionUpdate() {
+  if (debug) console.log('start versionUpdate');
+  const stats = await updateLocalStorageStats();
+  // update version.
+  stats.version = getVersion();
+  await saveStats(stats);
+  if (debug) console.log('stats updated.', stats);
+}
+
 
 // function get_csrf_token() {
 //   const csrfToken = document.querySelector('meta[name=csrf-token]').getAttribute('content');
