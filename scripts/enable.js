9f924420f85721dde7a7ff0e342b16cf5253a359
flaxinger <70012548+flaxinger@users.noreply.github.com>
2022-11-13 16:09:38 +0900

    feat: 제출 목록 언어 필터 추가 및 filter 함수 조건 객체화 (#155)
    * feature: change filter function and format code

--- scripts/enable.js
+++ scripts/enable.js
@@ -3,12 +3,12 @@
     가독성을 위해 따로 파일 분리함
 */
 async function checkEnable() {
-    const enable = await getObjectFromLocalStorage('bjhEnable');
-    if (!enable) writeEnableMsgOnLog();
-    return enable;
+  const enable = await getObjectFromLocalStorage('bjhEnable');
+  if (!enable) writeEnableMsgOnLog();
+  return enable;
 }
 
 function writeEnableMsgOnLog() {
-    const errMsg = "확장이 활성화되지 않았습니다. 확장을 활성화하고 시도해주세요";
-    console.log(errMsg)
+  const errMsg = '확장이 활성화되지 않았습니다. 확장을 활성화하고 시도해주세요';
+  console.log(errMsg);
 }
