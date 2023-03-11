0e4d1f865d3b4b92ce411448a414e4abaa663e6e
flaxinger <flaxinger@gmail.com>
2023-03-11 12:06:22 +0900

    chore: deprecate all upload feature

--- scripts/baekjoon/baekjoon.js
+++ scripts/baekjoon/baekjoon.js
@@ -16,18 +16,6 @@ const username = findUsername();
 if (!isNull(username)) {
   if (['status', `user_id=${username}`, 'problem_id', 'from_mine=1'].every((key) => currentUrl.includes(key))) startLoader();
   else if (currentUrl.match(/\.net\/problem\/\d+/) !== null) parseProblemDescription();
-  else if (currentUrl.includes('.net/user')) {
-    getStats().then((stats) => {
-      if (!isEmpty(stats.version) && stats.version === getVersion()) {
-        if (findUsernameOnUserInfoPage() === username) {
-          insertUploadAllButton();
-          insertDownloadAllButton();
-        }
-      } else {
-        versionUpdate();
-      }
-    });
-  }
 }
 
 function startLoader() {
