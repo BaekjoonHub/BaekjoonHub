02b1eb2e767694d246ef40626fbed6f4dd9b536f
Kyu0 <efftar@naver.com>
2023-05-17 01:17:23 +0900

    fix: compareSubmission function comment

--- scripts/baekjoon/util.js
+++ scripts/baekjoon/util.js
@@ -57,10 +57,11 @@ function startUploadCountDown() {
  * @param {object} a - 제출 요소 피연산자 a
  * @param {object} b - 제출 요소 피연산자 b
  * @returns {number} - a와 b 아래의 우선순위로 값을 비교하여 정수값을 반환합니다.
- * 1. 실행시간(runtime)의 차이가 있을 경우 그 차이 값을 반환합니다.
- * 2. 사용메모리(memory)의 차이가 있을 경우 그 차이 값을 반환합니다.
- * 3. 코드길이(codeLength)의 차이가 있을 경우 그 차이 값을 반환합니다.
- * 4. 위의 요소가 모두 같은 경우 제출한 요소(submissionId)의 그 차이 값의 역을 반환합니다.
+ * 1. 서브태스크가 있는 문제이고, 점수(result)의 차이가 있을 경우 a > b 이면 음수, a < b 이면 양수를 반환합니다.
+ * 2. 실행시간(runtime)의 차이가 있을 경우 그 차이 값을 반환합니다.
+ * 3. 사용메모리(memory)의 차이가 있을 경우 그 차이 값을 반환합니다.
+ * 4. 코드길이(codeLength)의 차이가 있을 경우 그 차이 값을 반환합니다.
+ * 5. 위의 요소가 모두 같은 경우 제출한 요소(submissionId)의 그 차이 값의 역을 반환합니다.
  * */
 function compareSubmission(a, b) {
   // prettier-ignore-start
