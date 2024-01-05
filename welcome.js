938639e205cfe802d5373955b6c2930f2da295f2
psrom <97152657+psrom@users.noreply.github.com>
2024-01-05 20:31:15 +0900

    fix: correct English grammar error
    관사를 수정했습니다.

--- welcome.js
+++ welcome.js
@@ -66,7 +66,7 @@ const createRepo = (token, name) => {
     name,
     private: true,
     auto_init: true,
-    description: 'This is a auto push repository for Baekjoon Online Judge created with [BaekjoonHub](https://github.com/BaekjoonHub/BaekjoonHub).',
+    description: 'This is an auto push repository for Baekjoon Online Judge created with [BaekjoonHub](https://github.com/BaekjoonHub/BaekjoonHub).',
   };
   data = JSON.stringify(data);
 
