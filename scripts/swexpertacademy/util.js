bb860680ba15c53451f363f989013a93d4dc5b63
getCurrentThread <wak8835@gmail.com>
2024-02-20 00:35:29 +0900

    fix: SWEA, 프로그래머스 문제 제출 시 시간 포멧팅이 잘못 나오는 오류 수정 (#246)

--- scripts/swexpertacademy/util.js
+++ scripts/swexpertacademy/util.js
@@ -89,5 +89,12 @@ function getNickname() {
  * @return {string} 포맷된 스트링
  */
 function getDateString(date){
-  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDay()}일 ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}:${seconds}`;
 }
