5d1f171b8966b795296d6215ad9f076c8937badd
mywhzy <cmj2393a@naver.com>
2024-08-20 23:41:03 +0900

    feat: 커밋 메세지에 난이도 추가 fix: 구름 레벨 문제 내 난이도 표시 element 변경에 따른 수정

--- scripts/goormlevel/parsing.js
+++ scripts/goormlevel/parsing.js
@@ -14,7 +14,8 @@ async function parseData() {
 
   const examSequence = Number(pathnameList[2]) || 0;
   const quizNumber = Number(pathnameList[5]) || 0;
-  const difficulty = document.querySelectorAll('#ratingTooltipTarget > div > label').length;
+  const difficultyLabel = document.querySelector('span[role=text] > span').innerHTML;
+  const difficulty = difficultyLabels[difficultyLabel];
 
   const titlePrefix = 'title-';
   const title = document.querySelector(`div[aria-label^="${titlePrefix}"]`).ariaLabel.replace(titlePrefix, '');
@@ -107,8 +108,8 @@ async function makeData({
   runtime,
 }) {
   const languageExtension = languages[language.toLowerCase()];
-  const directory = await getDirNameByOrgOption(`goormlevel/${examSequence}/${quizNumber}. ${convertSingleCharToDoubleChar(title)}`, language);
-  const message = `[goormlevel] Title: ${title}, Time: ${runtime}, Memory: ${memory}, Difficulty: ${difficulty} -BaekjoonHub`;
+  const directory = await getDirNameByOrgOption(`goormlevel/${difficulty}/${examSequence}/${quizNumber}. ${convertSingleCharToDoubleChar(title)}`, language);
+  const message = `[난이도 ${difficulty}] Title: ${title}, Time: ${runtime}, Memory: ${memory}, Difficulty: ${difficulty} -BaekjoonHub`;
   const fileName = `${convertSingleCharToDoubleChar(title)}.${languageExtension}`;
   const dateInfo = getDateString(new Date(Date.now()));
   // prettier-ignore
