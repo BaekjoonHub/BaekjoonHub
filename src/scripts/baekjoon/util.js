cbfb8e1cf036a91dfb4fd1591ae119737b1f4d50
getCurrentThread <wak8835@gmail.com>
2025-04-20 12:11:29 +0900

    refactor: improve code maintainability and reduce duplication
    - Create common UI utility module (ui-util.js) to eliminate duplicate code
    - Consolidate shared CSS styles into common.css
    - Enhance UploadService with better error handling and single file upload
    - Refactor platform-specific utils to use common modules

--- src/scripts/baekjoon/util.js
+++ src/scripts/baekjoon/util.js
@@ -1,62 +1,38 @@
+import { initUploadUI, markUploadedCSS as markUploaded, markUploadFailedCSS as markFailed, startUploadCountDown, convertImageTagToAbsoluteURL, getDateString } from '../ui-util.js';
+
 /**
  * 로딩 버튼 추가
  */
 function startUpload() {
-  let elem = document.getElementById('BaekjoonHub_progress_anchor_element');
-  if (elem !== undefined) {
-    elem = document.createElement('span');
-    elem.id = 'BaekjoonHub_progress_anchor_element';
-    elem.className = 'runcode-wrapper__8rXm';
-    elem.style = 'margin-left: 10px;padding-top: 0px;';
+  const target = document.getElementById('status-table')?.childNodes[1]?.childNodes[0]?.childNodes[3] || 
+                document.querySelector('div.table-responsive > table > tbody > tr > td:nth-child(5)');
+  const container = initUploadUI(target, uploadState);
+  
+  // 백준 사이트에 특화된 추가 로직이 필요한 경우 여기에 구현
+  if (target && target.childNodes.length > 0 && container) {
+    target.childNodes[0].append(container);
   }
-  elem.innerHTML = `<div id="BaekjoonHub_progress_elem" class="BaekjoonHub_progress"></div>`;
-  const target = document.getElementById('status-table')?.childNodes[1].childNodes[0].childNodes[3] || document.querySelector('div.table-responsive > table > tbody > tr > td:nth-child(5)');
-  target.append(elem);
-  if (target.childNodes.length > 0) {
-    target.childNodes[0].append(elem);
-  }
-  startUploadCountDown();
 }
 
 /**
  * 업로드 완료 아이콘 표시 및 링크 생성
- * @param {object} branches - 브랜치 정보 ('userName/repositoryName': 'branchName')
- * @param {string} directory - 디렉토리 정보 ('백준/Gold/1. 문제이름')
- * 1. 업로드 완료 아이콘을 표시합니다.
- * 2. 아이콘 클릭 시 업로드된 GitHub 링크로 이동하는 이벤트 리스너를 등록합니다.
+ * @param {object} branches - 브랜치 정보
+ * @param {string} directory - 디렉토리 정보
  */
 function markUploadedCSS(branches, directory) {
-  uploadState.uploading = false;
-  const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = 'markuploaded';
-  const uploadedUrl = "https://github.com/" +
-              Object.keys(branches)[0] + "/tree/" + 
-              branches[Object.keys(branches)[0]] + "/" + directory;
-  elem.addEventListener("click", function() {
-    window.location.href = uploadedUrl;
-  });
-  elem.style.cursor = "pointer";
+  markUploaded(branches, directory, uploadState);
 }
 
 /**
  * 업로드 실패 아이콘 표시
  */
 function markUploadFailedCSS() {
-  uploadState.uploading = false;
-  const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = 'markuploadfailed';
+  markFailed(uploadState);
 }
 
-/**
- * 총 실행시간이 10초를 초과한다면 실패로 간주합니다.
- */
-function startUploadCountDown() {
-  uploadState.uploading = true;
-  uploadState.countdown = setTimeout(() => {
-    if (uploadState.uploading === true) {
-      markUploadFailedCSS();
-    }
-  }, 10000);
+// convertImageTagAbsoluteURL 함수를 리팩토링된 공통 함수로 대체
+function convertImageTagAbsoluteURL(doc = document) {
+  convertImageTagToAbsoluteURL(doc);
 }
 
 /**
@@ -167,20 +143,4 @@ function convertImageTagAbsoluteURL(doc = document) {
   });
 }
 
-/**
- * 백준의 날짜 형식과 같게 포맷된 스트링을 반환하는 함수
- * @example 2023년 9월 23일 16:26:26
- * @param {Date} date
- * @return {string} 포맷된 스트링
- */
-function getDateString(date){
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}:${seconds}`;
-
-}
+// getDateString 함수는 이제 ui-util.js에서 import됩니다.
--- src/scripts/swexpertacademy/util.js
+++ src/scripts/swexpertacademy/util.js
@@ -1,22 +1,22 @@
+import { initUploadUI, markUploadedCSS as markUploaded, markUploadFailedCSS as markFailed, getDateString } from '../ui-util.js';
+
 /**
  * 로딩 버튼 추가
  */
 function startUpload() {
-  let elem = document.getElementById('BaekjoonHub_progress_anchor_element');
-  if (elem !== undefined) {
-    elem = document.createElement('span');
-    elem.id = 'BaekjoonHub_progress_anchor_element';
-    // elem.className = 'runcode-wrapper__8rXm';
-    // elem.style = 'margin-left: 10px;padding-top: 0px;';
-  }
-  elem.innerHTML = `<div id="BaekjoonHub_progress_elem" class="BaekjoonHub_progress"></div>`;
   const target = document.querySelector('div.box-list > div.box-list-inner > div.right_answer > span.btn_right');
   if (!isNull(target)) {
-    target.prepend(elem);
+    const container = initUploadUI(target, uploadState);
+    if (container) {
+      target.prepend(container);
+    }
   }
-  // start the countdown
-  startUploadCountDown();
 }
+
+/**
+ * SWEA 플랫폼에서 접근이 좋은 업로드 버튼 생성
+ * @param {string} link - 업로드 시 이동할 링크
+ */
 function makeSubmitButton(link) {
   let elem = document.getElementById('BaekjoonHub_submit_button_element');
   if (elem !== undefined) {
@@ -35,43 +35,18 @@ function makeSubmitButton(link) {
 
 /**
  * 업로드 완료 아이콘 표시 및 링크 생성
- * @param {object} branches - 브랜치 정보 ('userName/repositoryName': 'branchName')
- * @param {string} directory - 디렉토리 정보 ('백준/Gold/1. 문제이름')
- * 1. 업로드 완료 아이콘을 표시합니다.
- * 2. 아이콘 클릭 시 업로드된 GitHub 링크로 이동하는 이벤트 리스너를 등록합니다.
+ * @param {object} branches - 브랜치 정보
+ * @param {string} directory - 디렉토리 정보
  */
 function markUploadedCSS(branches, directory) {
-  uploadState.uploading = false;
-  const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = 'markuploaded';
-  const uploadedUrl = "https://github.com/" +
-              Object.keys(branches)[0] + "/tree/" + 
-              branches[Object.keys(branches)[0]] + "/" + directory;
-  elem.addEventListener("click", function() {
-    window.location.href = uploadedUrl;
-  });
-  elem.style.cursor = "pointer";
+  markUploaded(branches, directory, uploadState);
 }
 
 /**
  * 업로드 실패 아이콘 표시
  */
 function markUploadFailedCSS() {
-  uploadState.uploading = false;
-  const elem = document.getElementById('BaekjoonHub_progress_elem');
-  elem.className = 'markuploadfailed';
-}
-
-/**
- * 총 실행시간이 10초를 초과한다면 실패로 간주합니다.
- */
-function startUploadCountDown() {
-  uploadState.uploading = true;
-  uploadState.countdown = setTimeout(() => {
-    if (uploadState.uploading === true) {
-      markUploadFailedCSS();
-    }
-  }, 10000);
+  markFailed(uploadState);
 }
 
 /**
@@ -82,19 +57,4 @@ function getNickname() {
   return document.querySelector('#Beginner')?.innerText || document.querySelector('header > div > span.name')?.innerText || '';
 }
 
-/**
- * 백준의 날짜 형식과 같게 포맷된 스트링을 반환하는 함수
- * @example 2023년 9월 23일 16:26:26
- * @param {Date} date
- * @return {string} 포맷된 스트링
- */
-function getDateString(date){
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const seconds = date.getSeconds().toString().padStart(2, '0');
-
-  return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}:${seconds}`;
-}
+// getDateString 함수는 이제 ui-util.js에서 import됩니다.
