4ab06c4c6aaecc2f17e5ca510bddfce288a313b7
NERDHEAD <89120147+NERDHEAD-lab@users.noreply.github.com>
2025-08-09 10:33:36 +0900

    fix(package): 개발 환경 개선 및 패키지 업데이트 (#307)
    - Chrome 의존성 최적화 (chrome → @types/chrome)
    - ESLint v9 마이그레이션 진행 v8 -> v9
    - 주요 패키지 보안 업데이트 (ESLint, Webpack 등)

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   singleQuote: false,
   semi: true,
   useTabs: false,
   tabWidth: 2,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   printWidth: 200,
 };
